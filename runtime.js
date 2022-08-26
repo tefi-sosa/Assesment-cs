4372217a5ba93651d050771b67a68396cbcb758b
Estefania Sosa <estefi.monumental@gmail.com>
2022-08-26 12:12:29 -0400

    Initial commit

---
+++ runtime.js
@@ -0,0 +1,84 @@
+const perf = require('execution-time')();
+
+
+function doublerAppend(nums){
+
+    let new_nums = [];
+
+    for (let i=0; i<nums.length; i++){
+        let num = nums[i] * 2;
+        new_nums.push(num);
+    }
+
+}
+
+
+function doublerInsert(nums){
+
+    let new_nums = [];
+
+    for (let i=0; i<nums.length; i++){
+        let num = nums[i] * 2;
+        new_nums.unshift(num);
+    }
+
+}
+
+
+function getSizedArray(size){
+    let array = [];
+    for (let i=0; i<size; i++){
+        array.push(i);
+    }
+    return array
+}
+
+
+const tinyArray = getSizedArray(10);
+const smallArray = getSizedArray(100);
+const mediumArray = getSizedArray(1000);
+const largeArray = getSizedArray(10000);
+const extraLargeArray = getSizedArray(100000);
+
+
+
+// How long does it take to double every number in a given 
+// array? 
+
+// Results for the tinyArray
+// insert 122.1 μs
+// append 171.7 μs
+
+// Results for the smallArray
+// insert 164.2 μs
+// append 133.3 μs
+
+// Results for the mediumArray
+// insert 247 μs
+// append 156.2 μs
+
+// Results for the largeArray
+// insert 10.5167 ms
+// append 740.4 μs
+
+// Results for the extraLargeArray
+// insert 1.2417558 s
+// append 7.0502 ms
+
+
+
+// Try it with first function
+perf.start();                     // Starts timer
+doublerAppend(tinyArray);
+let resultsAppend = perf.stop();  // Stops timer and save time results
+
+
+// Try it with second function
+perf.start();
+doublerInsert(tinyArray);
+let resultsInsert = perf.stop();
+
+
+console.log('Results for the tinyArray');
+console.log("insert", resultsInsert.preciseWords);
+console.log("append", resultsAppend.preciseWords);
---
+++ step2.js
@@ -0,0 +1,80 @@
+/////// ADD TO ZERO ////////////
+
+// Time complexity O(n^2)
+// Space complexity O(1)
+
+function addToZero (numbers) {
+  let test = false;
+  for (let j = 0; j < numbers.length; j++) {
+      for (let i = (j + 1); i < numbers.length; i++) {
+          if (numbers[j] + (numbers[i]) === 0) {
+              console.log("True")
+              test = true
+              return true
+          }
+      }
+  };
+  if (test === false) {
+      console.log("False")
+  }
+}
+
+addToZero([]);
+// -> False
+
+addToZero([1]);
+// -> False
+
+addToZero([1, 2, 3]);
+// -> False
+
+addToZero([1, 2, 3, -2]);
+// -> True
+
+//////// HAS UNIQUE CHARACTERS ////////
+
+// Time complexity O(1)
+// Space complexity O(1)
+
+function hasUniqueChars (word) {
+  return new Set(word).size === word.length;
+}
+
+console.log(hasUniqueChars("Monday"));
+// -> True
+
+console.log(hasUniqueChars("Moonday"));
+// -> False
+
+/////////// PANGRAM SENTENCE  ///////////////
+
+//time complexity O(1)
+// space complexity O(1)
+
+function isPangram (arr) {
+  return new Set(arr.toLowerCase().match(/[a-z]/g)).size === 26
+}
+
+console.log(isPangram("The quick brown fox jumps over the lazy dog!"))
+// -> True
+
+console.log(isPangram("I like cats, but not mice"))
+// -> False
+
+////////// LONGEST WORD //////////////
+
+// time complexity O(n)
+// Space complexity O(1)
+
+function findLongestWord (arr) {
+  let count = 0
+  for (let i = 0; i < arr.length; i++) {
+    if ( arr[i].length > count) {
+      count = Number(arr[i].length)
+    }
+  }
+  return count
+}
+
+console.log(findLongestWord(["hi", "hello"]))
+// -> 5
